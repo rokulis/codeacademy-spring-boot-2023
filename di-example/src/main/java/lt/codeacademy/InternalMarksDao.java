4b90080b7d367521e524ee21f79fa1e82831ccfb
Rokas Ulickas <rokas@recodin.lt>
2023-09-06 21:01:06 +0300

    1 paskaita: 11 -> 1.8 java

--- di-example/src/main/java/lt/codeacademy/InternalMarksDao.java
+++ di-example/src/main/java/lt/codeacademy/InternalMarksDao.java
@@ -1,11 +1,12 @@
 package lt.codeacademy;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InternalMarksDao implements MarksDao {
 
     @Override
     public List<Integer> getMarks() {
-        return List.of(5, 8, 10, 10, 7, 6, 8);
+        return Arrays.asList(5, 8, 10, 10, 7, 6, 8);
     }
 }
